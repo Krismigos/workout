95b104b084482d5a44463885e315f002ca0b68c4
Krismigos <zeligans.1337@gmail.com>
2023-07-27 22:18:10 +0200

    Create main menu

--- javascript/index.js
+++ javascript/index.js
@@ -3,3 +3,17 @@ let menuBtn = document.querySelector(".burger");
 menuBtn.addEventListener("click", function () {
   menuBtn.classList.toggle("active");
 });
+
+// Знаходжу поточний розмір экрану щоб змінити бургер меню
+function displayScreenSize() {
+  const screenWidth = window.innerWidth;
+  let det = document.getElementById("navigation-menu");
+  if (screenWidth >= 1024) {
+    det.open = true;
+  } else {
+    det.open = false;
+  }
+}
+// слідкую за розміром єкрану
+window.addEventListener("resize", displayScreenSize);
+displayScreenSize();
